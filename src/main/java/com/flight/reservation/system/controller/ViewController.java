8dc69db514da1084632076f06acd78c0caced4ff
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-23 21:34:29 +0530

    add complete profile get method

--- src/main/java/com/flight/reservation/system/controller/ViewController.java
+++ src/main/java/com/flight/reservation/system/controller/ViewController.java
@@ -1,10 +1,21 @@
 package com.flight.reservation.system.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.flight.reservation.system.model.Passenger;
+import com.flight.reservation.system.repository.PassengerRepository;
 
 @Controller
 public class ViewController {
+	
+	@Autowired
+	PassengerRepository passengerRepository;
 	
 	@GetMapping("/")
 	public String home() {
@@ -19,5 +30,19 @@ public class ViewController {
 	@GetMapping("/sign-in")
 	public String signIn() {
 		return "sign-in";
+	}
+	
+	@GetMapping("/complete-profile/{id}")
+	public String completeProfile(@PathVariable Long id, Model model) {
+		Optional<Passenger> passengerOpt = passengerRepository.findById(id);
+	    
+	    if(passengerOpt.isPresent()) {
+	    	Passenger passenger = passengerOpt.get();
+	    	model.addAttribute("passenger", passenger);
+	    };
+	    
+	    
+		
+		return "complete-profile";
 	}
 }
