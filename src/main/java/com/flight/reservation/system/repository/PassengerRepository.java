809b95c0f37fce810bb8c2d9651c820d934e11af
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-24 18:10:16 +0530

    chore: delete unused imports

--- src/main/java/com/flight/reservation/system/repository/PassengerRepository.java
+++ src/main/java/com/flight/reservation/system/repository/PassengerRepository.java
@@ -3,7 +3,6 @@ package com.flight.reservation.system.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.flight.reservation.system.model.Passenger;
-import java.util.List;
 import java.util.Optional;
 
 
