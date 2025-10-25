6459e9e31ca35c14699c543f63a8e993ffd73f59
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-25 20:58:03 +0530

    add get methods

--- src/main/java/com/flight/reservation/system/controller/ViewController.java
+++ src/main/java/com/flight/reservation/system/controller/ViewController.java
@@ -1,5 +1,8 @@
 package com.flight.reservation.system.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,14 +11,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.flight.reservation.system.model.Flight;
 import com.flight.reservation.system.model.Passenger;
+import com.flight.reservation.system.model.Reservation;
+import com.flight.reservation.system.repository.FlightRepository;
 import com.flight.reservation.system.repository.PassengerRepository;
+import com.flight.reservation.system.repository.ReservationRepository;
 
 @Controller
 public class ViewController {
 	
 	@Autowired
 	PassengerRepository passengerRepository;
+	
+	@Autowired
+	ReservationRepository reservationRepository;
+	
+	@Autowired
+	FlightRepository flightRepository;
 	
 	@GetMapping("/")
 	public String homePage() {
@@ -50,8 +63,19 @@ public class ViewController {
 	    if(passengerOpt.isPresent()) {
 	    	Passenger passenger = passengerOpt.get();
 	    	model.addAttribute("passenger", passenger);
-	    };		
+	    };
 		return "profile";
+	}
+	
+	@GetMapping("/update-profile/{id}")
+	public String updateProfilePage(@PathVariable Long id, Model model) {
+		Optional<Passenger> passengerOpt = passengerRepository.findById(id);
+	    
+	    if(passengerOpt.isPresent()) {
+	    	Passenger passenger = passengerOpt.get();
+	    	model.addAttribute("passenger", passenger);
+	    };		
+		return "update-profile";
 	}
 	
 	@GetMapping("/search-flight/{id}")
@@ -65,21 +89,67 @@ public class ViewController {
 		return "search-flight";
 	}
 	
+	@GetMapping("/reservations/{id}")
+	public String reservationsPage(@PathVariable Long id, Model model) {
+		Optional<Passenger> passengerOpt = passengerRepository.findById(id);
+		List<Reservation> reservations = reservationRepository.findByPassengerId(id);
+		model.addAttribute("reservations", reservations);
+		
+		Map<Long, Flight> flightData = new HashMap<Long, Flight>();
+		for(Reservation reservation : reservations) {
+			Optional<Flight> flightOpt = flightRepository.findById(reservation.getFlightId());
+			if(flightOpt.isPresent()) {
+				flightData.put(reservation.getId(), flightOpt.get());
+			}
+		}
+		model.addAttribute("flightData", flightData);		
+		
+	    
+	    if(passengerOpt.isPresent()) {
+	    	Passenger passenger = passengerOpt.get();
+	    	model.addAttribute("passenger", passenger);
+	    };		
+		return "reservations";
+	}
+	
 	@GetMapping("review-reservation/{id}")
 	public String redirectPage(@PathVariable Long id) {
 		return "redirect:/search-flight/" + id;
 	}
 	
 	
-	@GetMapping("/checkout/{id}")
-	public String checkOutPage(@PathVariable Long id, Model model) {
-		Optional<Passenger> passengerOpt = passengerRepository.findById(id);
+	@GetMapping("/checkout/{passengerId}/{reservationId}")
+	public String checkOutPage(@PathVariable Long passengerId, @PathVariable Long reservationId, Model model) {
+		Optional<Passenger> passengerOpt = passengerRepository.findById(passengerId);
+		Optional<Reservation> reservationOpt = reservationRepository.findById(reservationId);
+	    
+	    if(reservationOpt.isPresent()) {
+	    	Reservation reservation = reservationOpt.get();
+	    	model.addAttribute("reservation", reservation);
+	    };	
 	    
 	    if(passengerOpt.isPresent()) {
 	    	Passenger passenger = passengerOpt.get();
 	    	model.addAttribute("passenger", passenger);
 	    };		
 		return "checkout";
+	}
+	
+	@GetMapping("/change-departure-date/{passengerId}/{reservationId}")
+	public String changeDepartureDatePage(@PathVariable Long passengerId, @PathVariable Long reservationId, Model model) {
+		Optional<Passenger> passengerOpt = passengerRepository.findById(passengerId);
+		Optional<Reservation> reservationOpt = reservationRepository.findById(reservationId);
+	    
+	    if(reservationOpt.isPresent()) {
+	    	Reservation reservation = reservationOpt.get();
+	    	model.addAttribute("reservation", reservation);
+	    }
+	    
+	    if(passengerOpt.isPresent()) {
+	    	Passenger passenger = passengerOpt.get();
+	    	model.addAttribute("passenger", passenger);
+	    }
+		return "change-departure-date";
 	}
 	
 	
