39e0d82e438fbfe0d8f0e50d4dc051d95ce4ece3
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-25 20:54:45 +0530

    add findByPassengerId method which returns list of passenger reservations

--- src/main/java/com/flight/reservation/system/repository/ReservationRepository.java
+++ src/main/java/com/flight/reservation/system/repository/ReservationRepository.java
@@ -3,7 +3,9 @@ package com.flight.reservation.system.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.flight.reservation.system.model.Reservation;
+import java.util.List;
+
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-
+	List<Reservation> findByPassengerId(long passengerId);
 }
