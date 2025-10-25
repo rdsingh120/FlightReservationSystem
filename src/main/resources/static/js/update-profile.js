1d9b559ceb3e1365f60d34be70f843f05fca9026
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-25 20:56:44 +0530

    add updateProfile page

---
+++ src/main/resources/static/js/update-profile.js
@@ -0,0 +1,102 @@
+const today = new Date().toISOString().split("T")[0];
+
+const firstNameInput = document.querySelector("#first-name");
+const firstNameInputError = document.querySelector("#first-name-error");
+
+const lastNameInput = document.querySelector("#last-name");
+const lastNameInputError = document.querySelector("#last-name-error");
+
+const emailInput = document.querySelector("#email");
+const emailInputError = document.querySelector("#email-error");
+
+const passwordInput = document.querySelector("#password");
+const passwordInputError = document.querySelector("#password-error");
+
+const nationalityInput = document.querySelector("#nationality");
+const nationalityInputError = document.querySelector("#nationality-error");
+
+const passportNumberInput = document.querySelector("#passport-number");
+const passportNumberInputError = document.querySelector("#passport-number-error");
+
+const issueDateInput = document.querySelector("#issue-date");
+const issueDateInputError = document.querySelector("#issue-date-error");
+
+const expiryDateInput = document.querySelector("#expiry-date");
+const expiryDateInputError = document.querySelector("#expiry-date-error");
+
+const phoneNumberInput = document.querySelector("#phone-number");
+const phoneNumberInputError = document.querySelector("#phone-number-error");
+
+const streetAddressInput = document.querySelector("#street-address");
+const streetAddressInputError = document.querySelector("#street-address-error");
+
+const cityInput = document.querySelector("#city");
+const cityInputError = document.querySelector("#city-error");
+
+const provinceInput = document.querySelector("#province");
+const provinceInputError = document.querySelector("#province-error");
+
+const postalCodeInput = document.querySelector("#postal-code");
+const postalCodeInputError = document.querySelector("#postal-code-error");
+
+const countryInput = document.querySelector("#country");
+const countryInputError = document.querySelector("#country-error");
+
+const saveChangesBtn = document.querySelector("#save-changes-btn");
+
+const setError = (input, inputError, errorMessage) => {
+  if (!input.value) {
+    inputError.textContent = errorMessage;
+    inputError.style.display = "block";
+    return true;
+  }
+  return false;
+};
+
+const removeError = (input, inputError) => {
+  input.addEventListener("input", () => {
+    inputError.textContent = "";
+    inputError.style.display = "none";
+  });
+};
+
+removeError(firstNameInput, firstNameInputError);
+removeError(lastNameInput, lastNameInputError);
+removeError(emailInput, emailInputError);
+removeError(passwordInput, passwordInputError);
+removeError(nationalityInput, nationalityInputError);
+removeError(passportNumberInput, passportNumberInputError);
+removeError(issueDateInput, issueDateInputError);
+removeError(expiryDateInput, expiryDateInputError);
+removeError(phoneNumberInput, phoneNumberInputError);
+removeError(streetAddressInput, streetAddressInputError);
+removeError(cityInput, cityInputError);
+removeError(provinceInput, provinceInputError);
+removeError(postalCodeInput, postalCodeInputError);
+removeError(countryInput, countryInputError);
+
+issueDateInput.max = today
+expiryDateInput.min = today
+
+saveChangesBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  var isValid = true;
+  if (setError(firstNameInput, firstNameInputError, "First name is required")) isValid = false;
+  if (setError(lastNameInput, lastNameInputError, "Last name is required")) isValid = false;
+  if (setError(emailInput, emailInputError, "email is required")) isValid = false;
+  if (setError(passwordInput, passwordInputError, "password is required")) isValid = false;
+  if (setError(nationalityInput, nationalityInputError, "Nationality is required")) isValid = false;
+  if (setError(passportNumberInput, passportNumberInputError, "Passport Number is required")) isValid = false;
+  if (setError(issueDateInput, issueDateInputError, "Issue Date is required")) isValid = false;
+  if (setError(expiryDateInput, expiryDateInputError, "Expiry Date is required")) isValid = false;
+  if (setError(phoneNumberInput, phoneNumberInputError, "Phone Number is required")) isValid = false;
+  if (setError(streetAddressInput, streetAddressInputError, "Street Address is required")) isValid = false;
+  if (setError(cityInput, cityInputError, "City is required")) isValid = false;
+  if (setError(provinceInput, provinceInputError, "Province/State is required")) isValid = false;
+  if (setError(postalCodeInput, postalCodeInputError, "Postal/Zip Code is required")) isValid = false;
+  if (setError(countryInput, countryInputError, "Country is required")) isValid = false;
+
+  if (isValid) {
+    document.querySelector("#update-profile-form").submit();
+  }
+});
