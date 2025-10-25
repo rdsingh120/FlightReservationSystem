5a2cd3a7f399afbff35efae6552529c0e8504d57
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-25 20:53:22 +0530

    add cancelReservation, changeDepartureDate, payment completed post methods

--- src/main/java/com/flight/reservation/system/controller/ReservationController.java
+++ src/main/java/com/flight/reservation/system/controller/ReservationController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -34,8 +35,8 @@ public class ReservationController {
 	public String reviewReservation(
 			@PathVariable Long id,
 			@RequestParam("flight") Long flightId,
-			@RequestParam("departureDate") String departureDate,
-			@RequestParam("adultsCount") int adultsCount,
+			@RequestParam String departureDate,
+			@RequestParam int adultsCount,
 			Model model) {
 		
 		Optional<Passenger> passengerOpt = passengerRepository.findById(id);
@@ -63,9 +64,50 @@ public class ReservationController {
 			@RequestParam LocalDate departureDate,
 			@RequestParam double totalPrice) {
 		
-		Reservation reservation = new Reservation(passengerId, flightId, departureDate, departureDate, adultsCount, totalPrice, BookingStatus.Pending);	
+		Reservation reservation = new Reservation(passengerId, flightId, LocalDate.now(), departureDate, adultsCount, totalPrice, BookingStatus.Pending);	
 		reservationRepository.save(reservation);
-		return "redirect:/checkout/" + reservation.getId();
+		return "redirect:/checkout/" + passengerId + "/" + reservation.getId();
+	}
+	
+	@PostMapping("/reservations/cancel/{passengerId}/{reservationId}")
+	public String cancelReservation(@PathVariable long passengerId, @PathVariable long reservationId) {
+		Optional<Reservation> reservationOpt = reservationRepository.findById(reservationId);
+		if(!reservationOpt.isPresent()) { return "redirect:/reservations/" + passengerId;}
+			Reservation reservation = reservationOpt.get();
+			reservation.setStatus(BookingStatus.Canceled);
+			reservationRepository.save(reservation);
+			return "redirect:/reservations/" + passengerId;		
+	}
+	
+	@PostMapping("/change-departure-date/{passengerId}/{reservationId}")
+	public String changeDepartureDate(@PathVariable Long passengerId, @PathVariable Long reservationId, @RequestParam LocalDate departureDate , Model model) {
+		Optional<Passenger> passengerOpt = passengerRepository.findById(passengerId);
+		Optional<Reservation> reservationOpt = reservationRepository.findById(reservationId);
+	    
+	    if(reservationOpt.isPresent()) {
+	    	Reservation reservation = reservationOpt.get();
+	    	if(reservation.getBookingDate().plusDays(10).isBefore(LocalDate.now())) {
+	    		return "redirect:/reservations/" + passengerId;
+	    	}
+	    	reservation.setDepartureDate(departureDate);
+	    	reservationRepository.save(reservation);
+	    }
+	    
+	    if(!passengerOpt.isPresent()) {}
+	    Passenger passenger = passengerOpt.get();
+	    model.addAttribute("passenger", passenger);
+	    
+		return "redirect:/reservations/" + passengerId;
+	}
+	
+	@PostMapping("/payment-completed/{passengerId}/{reservationId}")
+	public String paymentCompleted(@PathVariable long passengerId, @PathVariable long reservationId, Model model) {
+		Passenger passenger = passengerRepository.findById(passengerId).get();
+		model.addAttribute("passenger", passenger);
 		
+		Reservation reservation = reservationRepository.findById(reservationId).get();
+		reservation.setStatus(BookingStatus.Confirmed);
+		reservationRepository.save(reservation);
+		return "payment-completed";
 	}
 }
